6568a89e9e47a72112af9d26f551d45c3735fbcd
saptarshi <saptarshi822@gmail.com>
2026-07-05 22:45:03 +0530

    fix:removed wrong path import

--- Foodi_Frontend/foodi-shadcn/apps/web/app/layout.tsx
+++ Foodi_Frontend/foodi-shadcn/apps/web/app/layout.tsx
@@ -2,11 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 
 
 import "@workspace/ui/styles/globals.css" 
-import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@workspace/ui/lib/utils";
-import { Sidebar } from "lucide-react";
-import { SidebarProvider } from "@workspace/ui/components/sidebar";
-import AppSidebar from "@/components/shadcn/app-sidebar";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'})
 
